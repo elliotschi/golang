f9293a960e4182d0d64e01e0d877a8f481cf1fb1
Elliot Chi <elliotschi@gmail.com>
2016-04-01 16:39:08 -0700

    feat: edit main.go

--- src/mathapp/main.go
+++ src/mathapp/main.go
@@ -6,6 +6,29 @@ import (
     "fmt"
 )
 
+// func main() {
+//     fmt.Printf("Hello, world. Sqrt(2) = %v\n", mymath.Sqrt(2))
+// }
+
+func max(a, b int) int {
+  if a > b {
+    return a
+  } else {
+    return b
+  }
+}
+
 func main() {
-    fmt.Printf("Hello, world. Sqrt(2) = %v\n", mymath.Sqrt(2))
+  x := 3
+  y := 4
+  z := 5
+  
+  maxXy := max(x, y)
+  maxXz := max(x, z)
+  
+  fmt.Printf("max(%d, %d) = %d\n", x, y, maxXy)
+  
+  fmt.Printf("max(%d, %d) = %d\n", x, z, maxXz)
+  
+  fmt.Printf("max(%d, %d) = %d\n", y, z, max(y, z))
 }
