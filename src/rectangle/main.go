0abb75f95cce99fabacca704bb67001941b2fdbd
Elliot Chi <elliotschi@gmail.com>
2016-04-02 19:06:45 -0700

    feat: add methods to rect and circle

---
+++ src/rectangle/main.go
@@ -0,0 +1,42 @@
+package main
+import (
+  "fmt"
+  "math"
+)
+
+type Rectangle struct {
+  width, height float64
+}
+
+type Circle struct {
+  radius float64
+}
+
+// func area(r Rectangle) float64 {
+//   return r.width*r.height
+// }
+
+func (r Rectangle) area() float64 {
+  return r.width*r.height
+}
+
+func (c Circle) area() float64 {
+  return math.Pi * c.radius * c.radius
+}
+
+func main() {
+  r1 := Rectangle{5,3}
+  r2 := Rectangle{2,5}
+  
+  c1 := Circle{1}
+  c2 := Circle{2}
+  
+  // fmt.Println("area of r1 is: ", area(r1))
+  // fmt.Println("area of r2 is: ", area(r2))
+  
+  fmt.Println("area of r1 is: ", r1.area())
+  fmt.Println("area of r2 is: ", r2.area())
+  fmt.Println("area of c1 is: ", c1.area())
+  fmt.Println("area of c2 is: ", c2.area())
+ 
+}
