48fed18ec4cb3d1ad33ee5592f19d317db7617a0
Elliot Chi <elliotschi@gmail.com>
2016-04-04 14:01:27 -0700

    feat: concurrency

---
+++ src/channels/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func sum(a []int, c chan int) {
+  total := 0
+  
+  for _, v := range a {
+    total += v
+  }
+  
+  c <- total
+  // send total to c the channel
+}
+
+func main() {
+  a := []int{7, 2, 8, -9, 4, 0}
+  
+  c := make(chan int)
+  go sum(a[:len(a)/2], c) // slice from 0 to the length /2 => first half
+  go sum(a[len(a)/2:], c) // sum of second half
+  
+  x, y := <-c, <-c // receive from channel c 
+  
+  fmt.Println(x, y, x + y)
+}
