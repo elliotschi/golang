62daacf4ec227ebf7e7d7e9f50ad90a82c84f790
Elliot Chi <elliotschi@gmail.com>
2016-04-05 10:14:55 -0700

    feat: file upload

--- src/web-server/main.go
+++ src/web-server/main.go
@@ -6,6 +6,11 @@ import (
   "net/http"
   "strings"
   "log"
+  "time"
+  "crypto/md5"
+  "io"
+  "strconv"
+  "os"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -30,10 +35,23 @@ func login(w http.ResponseWriter, r *http.Request) {
   fmt.Println("method: ", r.Method)
   
   if r.Method == "GET" {
+    crutime := time.Now().Unix()
+    h := md5.New()
+    io.WriteString(h, strconv.FormatInt(crutime, 10))
+    token := fmt.Sprintf("%x", h.Sum(nil))
+    
     t, _ := template.ParseFiles("login.gtpl")
-    t.Execute(w, nil)
+    t.Execute(w, token)
   } else {
     r.ParseForm()
+    
+    token := r.Form.Get("token")
+    
+    if token != "" {
+      
+    } else {
+      
+    }
     
     fmt.Println("username: ", template.HTMLEscapeString(r.Form.Get("username")))
     
@@ -43,10 +61,40 @@ func login(w http.ResponseWriter, r *http.Request) {
   }
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("method:", r.Method)
+  if r.Method == "GET" {
+    crutime := time.Now().Unix()
+    h := md5.New()
+    io.WriteString(h, strconv.FormatInt(crutime, 10))
+    token := fmt.Sprintf("%x", h.Sum(nil))
+
+    t, _ := template.ParseFiles("upload.gtpl")
+    t.Execute(w, token)
+  } else {
+    r.ParseMultipartForm(32 << 20)
+    file, handler, err := r.FormFile("uploadfile")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer file.Close()
+    fmt.Fprintf(w, "%v", handler.Header)
+    f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer f.Close()
+    io.Copy(f, file)
+  }
+}
+
 func main() {
   // set route
   http.HandleFunc("/", sayhelloName) 
   http.HandleFunc("/login", login)
+  http.HandleFunc("/upload", upload)
   // listen to port number
   err := http.ListenAndServe(":9090", nil)
   
