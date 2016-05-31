20da429bb29dd890ab81375f976858d9968fbe95
Elliot Chi <elliotschi@gmail.com>
2016-05-31 00:30:27 -0700

    feat: hash table

---
+++ src/data-structures/hash-table/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"  
+)
+
+func main() {
+  n := HashBucket("Go", 12)
+  
+  fmt.Println(n)  
+}
+
+func HashBucket(word string, buckets int) int {
+  letter := int(word[0])
+  
+  bucket := letter % buckets
+  
+  return bucket
+}
--- src/moby/main.go
+++ src/moby/main.go
@@ -2,27 +2,25 @@ package main
 
 import (
   "fmt"
-  "io"
-  "bufio"
-  "net/http"
-  "os"
+  "io/ioutil"
   "log"
+  "net/http"
 )
 
 func main() {
-  res, err := http.Get("")
+  res, err := http.Get("https://www.gutenberg.org/files/2701/old/moby10b.txt")
   
   if err != nil {
     log.Fatalln(err)
   }
   
-  scanner := bufio.NewScanner(res.Body)
-  defer res.Body.Close()
+  bs, err := ioutil.ReadAll(res.Body)
   
-  buckets := make([]int, 200)
+  res.Body.Close()
   
-  for scanner.Scan() {
-    n := HashBucket(scanner.Text())   
-    buckets[n]++
+  if err != nil {
+    log.Fatal(err)
   }
+  
+  fmt.Printf("%s", bs)
 }
